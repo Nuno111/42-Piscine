73e529c447b5cf4b39488aadaf75feaa7b2b0bf2
unknown <nuno_c11@hotmail.com>
2021-01-13 10:17:36 +0000

    finished c03 ex1 and ex2

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -2,5 +2,10 @@
 
 int     ft_strcmp(char *s1, char *s2)
 {
-    
+    int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && (unsigned char)s1[i] == (unsigned char)s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
---
+++ c03/ex01/ft_strncmp.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int     ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    int i;
+
+    if (n == 0)
+    {
+        return (0);
+    }
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && (unsigned char)s1[i] == (unsigned char)s2[i] && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
