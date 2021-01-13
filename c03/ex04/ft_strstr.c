7d17660997e6166d59951886dbced0f1cfc176a7
unknown <nuno_c11@hotmail.com>
2021-01-13 12:12:42 +0000

    reviewed ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -7,33 +7,25 @@ char *ft_strstr(char *str, char *to_find)
 
     i = 0;
     j = 0;
+    if (to_find[0] == '\0')
+    {  
+        return (str);
+    }
     while (str[i] != '\0')
     {
         if (str[i] == to_find[j])
         {
+            j = 0;
             while (str[i + j] == to_find[j] && to_find[j] != '\0')
             {
                 j++;
             }
             if (to_find[j] == '\0')
             {
-                write(1, "a", 1);
                 return (&str[i]);
-            }
-            else
-            {
-                j = 0;
             }
         }
         i++;
     }
     return (0);
-}
-
-int main(void)
-{
-    char a[] = "Hello";
-    char b[] = "ell";
-
-    ft_strstr(&a[0], &b[0]);
 }
