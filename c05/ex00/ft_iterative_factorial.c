8c409d4c2ff77fb09de16014d636da8fd02b64cf
Nuno Gregorio <ngregori@c1r8s6.42lisboa.com>
2021-01-19 21:43:14 +0000

    a

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngregori <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 21:25:04 by ngregori          #+#    #+#             */
+/*   Updated: 2021/01/19 21:42:26 by ngregori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int ft_iterative_factorial(int nb)
 {
     int i;
 
-	if (nb == 0)
+	if (nb == 0 || nb == 1)
 		return (1);
     if (nb < 0)
         return (0);
@@ -11,4 +23,10 @@ int ft_iterative_factorial(int nb)
         nb *= i;
 
     return nb;
+}
+
+int main(void)
+{
+	int b = ft_iterative_factorial(10);
+	printf("%d", b);
 }
